3b8e07a912859bfe39c9a202467d71b5db062aca
yuushigure27 <yuushigure1227@gmail.com>
2024-04-09 13:37:30 +0000

    投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'lists/show'
   get 'lists/edit'
   get 'top' => 'homes#top'
+  
+  post 'lists' => 'lists#create' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
