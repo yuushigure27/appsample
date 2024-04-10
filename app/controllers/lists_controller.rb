41011fc6de05cba4b177d361c1d9212db9a91d41
yuushigure27 <yuushigure1227@gmail.com>
2024-04-10 17:59:15 +0000

    削除機能追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,11 +19,24 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
   end
   
    private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
