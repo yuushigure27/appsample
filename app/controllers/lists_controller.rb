04a7c1b471fd1dee4fa68f86042d64f4493f418f
yuushigure27 <yuushigure1227@gmail.com>
2024-04-10 03:09:24 +0000

    投稿すると詳細画面に移動

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,17 +5,17 @@ class ListsController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
+  
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])  
   end
 
   def edit
